905a514faeba5534f83932920214b5330c4b6aa2
muhannadsayedabdullah <symohannad@gmail.com>
2023-03-22 12:20:07 +0100

    build-game-logic

--- main.py
+++ main.py
@@ -3,3 +3,16 @@
 word = 'SWEDEN'
 reveal = list(len(word)*'_')
 print(reveal)
+
+lives = 7
+gameDone = False
+while gameDone == False:
+    guess = input('Gissa en bokstav ... Oops ordet är på engelska')
+    guess = guess.upper()
+
+    if guess == word:
+        gameDone = True
+if gameDone:
+    print('WOHOO , DET ÄR KLART ')
+else : 
+    print('Tyvärr!!!! ordet är : ' , word)
