f4d3f9d78b3b44d0eedbab6e163c054539cdea23
muhannadsayedabdullah <symohannad@gmail.com>
2023-03-22 12:59:04 +0100

    words-logic

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 # here we start 
 
+from words import selected_word
+
+
 word = 'SWEDEN'
+# word = selected_word()
 reveal = list(len(word)*'_')
-print(reveal)
+
 
 lives = 7
 gameDone = False
-while gameDone == False:
+while gameDone == False and lives > 0:
+    print(reveal)
     guess = input('Gissa en bokstav ... Oops ordet är på engelska')
     guess = guess.upper()
 
     if guess == word:
         gameDone = True
+    if len(guess) == 1 and guess in word:
+        for i in range(0,len(word)):
+            letter = word[i]
+            if guess == letter:
+                reveal[i] = guess
+        if '_' not in reveal:
+            gameDone = True
+    else : 
+        lives -=1
 if gameDone:
     print('WOHOO , DET ÄR KLART ')
 else : 
